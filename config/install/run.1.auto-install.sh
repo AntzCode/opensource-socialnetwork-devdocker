8c42406b41f40fb86edc05dd940da2d5f04e183a
AntzCode Ltd <biz@antzcode.com>
2021-08-05 13:09:43 +1200

    Improvements to installer: one-line install!  - Request elevated privileges if required  - Autogenerate entries in hosts file  - Automatically chown files  - Option for bypassing Automated Installer  - Option for providing a custom project url  - Option for showing verbose output of the progress  - A help section describing the options
    Eg: ./install.sh -h

--- config/install/run.1.auto-install.sh
+++ config/install/run.1.auto-install.sh
@@ -33,10 +33,10 @@ replaceMyVar(){
 VARS=(MARIADB_DATABASE MARIADB_PASSWORD MARIADB_USER MARIADB_HOST WEBSITE_NAME CONTACT_EMAIL OSSN_DATA_DIRECTORY
 ADMIN_FIRSTNAME ADMIN_LASTNAME ADMIN_EMAIL ADMIN_USERNAME ADMIN_PASSWORD ADMIN_BIRTH_DAY ADMIN_BIRTH_MONTH ADMIN_BIRTH_YEAR ADMIN_GENDER)
 
+log v "Compiling settings from .env into the Automated Installation Template files..."
 SETTINGS_FILE_CONTENTS=$(cat ./1.auto-install/settings.php)
 ACCOUNT_FILE_CONTENTS=$(cat ./1.auto-install/account.php)
 INSTALLED_FILE_CONTENTS=$(cat ./1.auto-install/installed.php)
-
 
 for i in "${VARS[@]}"
 do
@@ -51,11 +51,13 @@ echo "$ACCOUNT_FILE_CONTENTS" > ./1.auto-install/account-compiled.php
 echo "$INSTALLED_FILE_CONTENTS" > ./1.auto-install/installed-compiled.php
 
 # create backup of original OSSN files before we replace them
+log v "Creating backups of the default OSSN installation template files..."
 mv ../../www/installation/pages/settings.php ../../www/installation/pages/settings.orig.php
 mv ../../www/installation/pages/account.php ../../www/installation/pages/account.orig.php
 mv ../../www/installation/pages/installed.php ../../www/installation/pages/installed.orig.php
 
 # replace the original install pages with the auto-submitting forms
+log v "Replacing the OSSN installation templates with the compiled Automated Installer templates..."
 cp ./1.auto-install/settings-compiled.php ../../www/installation/pages/settings.php
 cp ./1.auto-install/account-compiled.php ../../www/installation/pages/account.php
 cp ./1.auto-install/installed-compiled.php ../../www/installation/pages/installed.php
@@ -65,6 +67,7 @@ cp ./1.auto-install/logo.svg ../../www/installation/styles/antzcode-logo.svg
 cp ./1.auto-install/background.svg ../../www/installation/styles/antzcode-background.svg
 
 # cleanup temp files
+log v "Removing temporary files..."
 rm ./1.auto-install/settings-compiled.php
 rm ./1.auto-install/account-compiled.php
 rm ./1.auto-install/installed-compiled.php
--- install.sh
+++ install.sh
@@ -15,59 +15,269 @@
 # Comment-out the following line to enable the script
 # echo "this script is automatically disabled after installation to prevent accidental data loss" && exit
 
-# TL;DR
-# INSTRUCTIONS: Read the following code block and then copy/paste it into your bash terminal.
-# You will be prompted to enter your password up front in order to execute the two elevated commands:
 
-# sudo echo
-# sudo echo "127.0.0.1 ossn.loc www.ossn.loc" >> /etc/hosts
-# git clone https://github.com/antzcode/opensource-socialnetwork-devdocker.git ossn.loc
-# cd ossn.loc
-# rm -rf www database/data/* ossn_data/*
-# git clone https://github.com/antzcode/opensource-socialnetwork.git www
-# sudo chown -R 33:docker www ossn_data
-# ./install.sh
-# ./start.sh
+############################################################
+# TL;DR -- how to use this file:
+############################################################
+#
+#   Read the help section for details:
+#     ./install.sh -h
+#
+#   The easiest way is to run it with the -a flag. This creates an entry in
+#    the hosts file, downloads the project and chowns the files:
+#      ./install.sh -a
+#
+#   Start the servers like this:
+#      ./start.sh
+#
+#   Stop the servers like this:
+#      ./stop.sh
+#
+############################################################
 
-if [ ! -f .env ]; then
-    # we will copy the sample .env file for them
-    cp .env.sample .env
-fi
 
+############################################################
+# Print the help
+############################################################
+
+Help()
+{
+	# Display Help
+	echo " ## "
+	echo " ##  Installs Open Source Social Network on the AntzCode DevDocker."
+	echo " ## "
+	echo " ##       Syntax: ./install.sh [-h|v|V|a|c|m|u]"
+	echo " ## "
+	echo " ##       Example:     ./install.sh -av    "
+	echo " ## "
+	echo " ##       Options:"
+	echo " ## "
+	echo " ##   h   Print this Help.    "
+  echo " ##   v   Verbose mode.    "
+  echo " ##   V   Print software version.    "
+	echo " ##   a   Automatic installation. * (requires elevated privileges)"
+	echo " ##        - Adds entries to the hosts file if not already exists"
+	echo " ##          and chowns the files."
+	echo " ##        - Is not the inverse of -m    "
+	echo " ##   c   Chown the files.        * (requires elevated privileges)    "
+	echo " ##   m   Manual setup.    "
+	echo " ##        - Does not install the Automated Installer but instead"
+  echo " ##          keeps OSSN's default two-step setup forms.    "
+	echo " ##   u   URL to the repository."
+	echo " ##        - Default: $GIT_PROJECT_URL"
+	echo " ##        - Some options might not work with other repositories.    "
+	echo " ## "
+}
+
+
+############################################################
+# Print Verbose output
+############################################################
+log()
+{
+	if [ "$1" == "v" ]; then
+		if [ "$doVerbose" == "true" ]; then
+			echo "$2"
+		fi
+	fi
+}
+
+
+############################################################
+############################################################
+###                                                      ###
+###               Main program begins                    ###
+###                                                      ###
+############################################################
+############################################################
+
+# The file that will be extracted into /www
 GIT_PROJECT_URL=https://github.com/antzcode/opensource-socialnetwork.git
 
-if [ ! -f ./www/installation ]; then
-    # let us download the project from Github just in case this is one who has not read the instructions at all
-    rm -rf www
-    git clone "${GIT_PROJECT_URL}" www
+# The hosts file on the host OS
+HOSTS_FILE=/etc/hosts
 
-    # attempting to chown the files
-    chown -R 33:docker www ossn_data
-    echo ""
-    echo "###################################################"
-    echo "###################################################"
-    echo "####                                           ####"
-    echo " ##      !!!!!  Important Message  !!!!!        ##"
-    echo " ##                                             ##"
-    echo " ##      You may need elevated privileges       ##"
-    echo " ##        to chown the webserver files         ##"
-    echo " ##                                             ##"
-    echo " ##        Execute the following command        ##"
-    echo " ##         Before you start the server:        ##"
-    echo " ##                                             ##"
-    echo " ##    sudo chown -R 33:docker www ossn_data    ##"
-    echo " ##                                             ##"
-    echo "###################################################"
-    echo "###################################################"
-    echo "###################################################"
-    echo ""
+# The local domains where the installation will run
+HOSTS_DOMAIN="ossn.loc www.ossn.loc"
+
+# IP of localhost for routing in the hosts file
+HOSTS_IP="127.0.0.1"
+
+
+############################################################
+# Process the input options
+############################################################
+
+# Initialize switch conditions
+doAuto=false
+doHosts=false
+doChown=false
+doManual=false
+doVerbose=false
+hasSudo=false
+requiresElevatedPrivileges=false
+
+# Get the options
+while getopts ":vhVacmu:" option; do
+   case $option in
+      v) # verbose output
+         doVerbose=true
+         ;;
+      h) # display Help
+         Help
+         exit;;
+      V) # display software version
+         echo "AntzCode OSSN Dev Docker Installer v1.0 - 2021"
+         exit;;
+      a) # automatic
+         doAuto=true
+         doHosts=true
+         doChown=true
+         requiresElevatedPrivileges=true
+         echo -n "You have chosen the automatic installation. "
+         ;;
+      c) # do the chown
+         if [ "$doAuto" == "false" ]; then
+	         doChown=true
+		 requiresElevatedPrivileges=true
+		 echo -n "You have chosen to chown the files. "
+         fi
+         ;;
+      m) # manual setup process
+         doManual=true
+         log v "Manual setup process (using the default OSSN configuration forms)"
+         ;;
+      u) # enable a custom GIT url
+         GIT_PROJECT_URL=$OPTARG
+         exit;;
+      \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+
+############################################################
+# Obtain elevated privileges
+############################################################
+
+if [ "$requiresElevatedPrivileges" == "true" ]; then
+	# need to check if they have elevated privileges
+	echo "Elevated privileges are required."
+	if sudo true; then
+		# has already logged in with sudo
+		hasSudo=true
+	else
+		echo 'Will not proceed while having insufficient privileges.'
+		exit 1
+	fi
 fi
 
-# run the pre-install scripts
-cd config/install
-./run.1.auto-install.sh
 
-# finished running install scripts
-cd ../..
+############################################################
+# Add entry to hosts file
+############################################################
+
+if [ "$doHosts" == "true" ]; then
+	if ! grep --quiet "$HOSTS_DOMAIN" "$HOSTS_FILE"; then
+		log v "adding entry to $HOSTS_FILE"
+		echo "## AntzCode/opensource-socialnetwork-devdocker begin " | sudo tee -a "$HOSTS_FILE" > /dev/null
+		echo "$HOSTS_IP	$HOSTS_DOMAIN" | sudo tee -a "$HOSTS_FILE" > /dev/null
+		echo "## AntzCode/opensource-socialnetwork-devdocker end " | sudo tee -a "$HOSTS_FILE" > /dev/null
+	else
+		log v "There is already an entry for $HOSTS_DOMAIN in $HOSTS_FILE, so no changes were made to it."
+	fi 
+fi
+
+
+############################################################
+# Create default .env file
+############################################################
+
+if [ -f .env ]; then
+	log v "An .env file exists already, we will use that."
+else
+	# we will copy the sample .env file for them
+	log v "Creating .env from .env.sample..."
+	cp .env.sample .env
+fi
+
+
+############################################################
+# Install the project files
+############################################################
+
+if [ ! -f ./www/installation ]; then
+	# let us download the project from Github just in case this is one who has not read the instructions at all
+
+	log v "www/installation directory does not exist, therefore the project will be downloaded."
+	log v "deleting the www directory..."
+
+	if [ "$hasSudo" == "true" ]; then 
+		sudo rm -rf www
+	else
+		rm -rf www
+	fi
+
+	git clone "${GIT_PROJECT_URL}" www
+
+	if [ "$doChown" == "true" ]; then
+		# attempting to chown the files
+		log v "Chowning the files..."
+		sudo chown -R 33:docker www ossn_data
+	else
+		# tell them that they need to chown the files
+		echo ""
+		echo "###################################################"
+		echo "###################################################"
+		echo "####                                           ####"
+		echo " ##      !!!!!  Important Message  !!!!!        ##"
+		echo " ##                                             ##"
+		echo " ##      You may need elevated privileges       ##"
+		echo " ##        to chown the webserver files         ##"
+		echo " ##                                             ##"
+		echo " ##        Execute the following command        ##"
+		echo " ##         Before you start the server:        ##"
+		echo " ##                                             ##"
+		echo " ##    sudo chown -R 33:docker www ossn_data    ##"
+		echo " ##                                             ##"
+		echo "###################################################"
+		echo "###################################################"
+		echo "###################################################"
+		echo ""
+	fi
+fi
+
+
+############################################################
+# Configure Automated Installer
+############################################################
+
+if [ "$doManual" == "false" ]; then
+	
+	log v "Configuring AntzCode automated installer..."
+
+	# install the AntzCode automated installer scripts
+	cd config/install
+	source ./run.1.auto-install.sh
+
+	# finished running install scripts
+	cd ../..
+
+fi
+
+
+############################################################
+# Finish
+############################################################
+
+log v "Installation finished, now disabling the install script..."
 
 sed -i '16s/.*/echo "this script is automatically disabled after installation to prevent accidental data loss" \&\& exit/' ./install.sh
+
+echo "All done."
+log v "You can start the server like this: ./start.sh"
+log v "You can stop the server like this: ./stop.sh"
+log v "You can even create a custom launcher to do this with a mouse click!"
+echo "To run this installer again, you will need to re-enable it by changing line 16 into a comment."
+
